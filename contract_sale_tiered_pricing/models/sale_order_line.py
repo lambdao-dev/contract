eea8e437581a694c92ecc96a789e44563892db5f
len <nans.lefebvre@gmail.com>
2023-06-26 16:34:47 +0200

    [FIX] contract_sale_tiered_pricing: format date in description

--- contract_sale_tiered_pricing/models/sale_order_line.py
+++ contract_sale_tiered_pricing/models/sale_order_line.py
@@ -92,7 +92,7 @@ class SaleOrderLine(models.Model):
         if cumulated_qty:
             lang_code = self.order_id.partner_id.lang
             qweb_date = self.env["ir.qweb.field.date"].with_context(lang=lang_code)
-            contract_end = line.date_end
+            contract_end = qweb_date.value_to_html(line.date_end, {})
             msg_tmp = _(
                 "History = {:.0f} (main contract running until {}), "
                 "new order of {:.0f} units from {} to {} on pricelist {}."
